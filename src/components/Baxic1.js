b074d368c20630ce1cd82d9486b97168adb7e9b1
yuki-kouno <zgoovw@gmail.com>
2021-06-25 09:29:55 +0900

    commit

--- src/components/Baxic1.js
+++ src/components/Baxic1.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Baxic1 = () => {
-  return <div></div>;
+const Baxic1 = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <button onClick={clickHandler}>Click</button>
+      <h1>Hello {props.nema}</h1>
+    </>
+  );
 };
 
 export default Baxic1;
